43bb49aa6e058190156bd4b1cb9881c8e10126ec
edu82 <eduardo@philipsoft.cl>
2023-02-13 15:47:00 -0300

    Firzt Kommit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../ui"
+import { LoginPage } from "../auth"
+import { DCPage, MarvelPage } from "../heroes"
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DCPage />} />
+
+                <Route path="login" element={<LoginPage />} />
+                <Route path="/" element={<Navigate to="/marvel" />} />
+            </Routes>
+        </>
+    )
+}
