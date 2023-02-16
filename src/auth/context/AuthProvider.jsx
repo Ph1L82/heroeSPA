22db030558252c6b6f4c050eff2f9a04aa617e56
edu82 <eduardo@philipsoft.cl>
2023-02-16 16:08:20 -0300

    logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -32,11 +32,19 @@ export const AuthProvider = ({ children }) => {
 
         localStorage.setItem('user', JSON.stringify(user));
 
-        dispatch(action)
+        dispatch(action);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = {
+            type: types.logout
+        };
+        dispatch(action);
     }
 
     return (
-        <AuthContext.Provider value={{ ...authState, login: login }}>
+        <AuthContext.Provider value={{ ...authState, login: login, logout: logout }}>
             {children}
         </AuthContext.Provider>
     )
