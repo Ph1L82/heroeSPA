e44542b381731683b6ca82b4bf97a828d6573382
edu82 <eduardo@philipsoft.cl>
2023-02-16 16:45:09 -0300

    proteccion de rutas publicas, no acceder si esta logueado.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,18 @@ import { Navbar } from "../ui"
 import { LoginPage } from "../auth"
 import { HeroesRoutes } from "../heroes"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path="login" element={<LoginPage />} />
+                <Route path="login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
                 <Route path="/*" element={
                     <PrivateRoute>
                         <HeroesRoutes />
