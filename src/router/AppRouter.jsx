3ee727abd01695639523d9eb6875c8e01ce3d9ad
edu82 <eduardo@philipsoft.cl>
2023-02-16 16:32:03 -0300

    aplica highorder component ProvateRoute para aplicar validacion logged en rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Navbar } from "../ui"
 import { LoginPage } from "../auth"
 import { HeroesRoutes } from "../heroes"
+import { PrivateRoute } from "./PrivateRoute"
 
 export const AppRouter = () => {
     return (
@@ -9,7 +10,12 @@ export const AppRouter = () => {
             <Navbar />
             <Routes>
                 <Route path="login" element={<LoginPage />} />
-                <Route path="/*" element={<HeroesRoutes />} />
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }
+                />
             </Routes>
         </>
     )
