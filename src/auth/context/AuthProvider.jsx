8be8a7e02f10e416eda10a05947ec4d78567a97e
edu82 <eduardo@philipsoft.cl>
2023-02-16 16:01:33 -0300

    login localstorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,20 +7,31 @@ const initialState = {
     logged: false,
 }
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return {
+        logged: !!user,
+        user: user,
+    }
+}
 
 export const AuthProvider = ({ children }) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
     const login = async (name = '') => {
+        const user = {
+            id: 1,
+            name: name
+        }
         const action = {
 
             type: types.login,
-            payload: {
-                id: 1,
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify(user));
+
         dispatch(action)
     }
 
